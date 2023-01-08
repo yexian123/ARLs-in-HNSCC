06fc692fa32aebb52e93607cb5c3afcd75f07539
Gaoqing <122167973+yexian123@users.noreply.github.com>
2023-01-08 14:08:49 +0800

    Add files via upload

---
+++ 21.ssGSEA.R
@@ -0,0 +1,44 @@
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("limma")
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("GSVA")
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("GSEABase")
+
+
+
+library(GSVA)
+library(limma)
+library(GSEABase)
+
+inputFile="symbol.txt"     
+gmtFile="immune.gmt"       
+setwd("C:\\22.ssGSEA")     
+
+
+rt=read.table(inputFile,sep="\t",header=T,check.names=F)
+rt=as.matrix(rt)
+rownames(rt)=rt[,1]
+exp=rt[,2:ncol(rt)]
+dimnames=list(rownames(exp),colnames(exp))
+mat=matrix(as.numeric(as.matrix(exp)),nrow=nrow(exp),dimnames=dimnames)
+mat=avereps(mat)
+mat=mat[rowMeans(mat)>0,]
+
+
+geneSet=getGmt(gmtFile, geneIdType=SymbolIdentifier())
+
+
+ssgseaScore=gsva(mat, geneSet, method='ssgsea', kcdf='Gaussian', abs.ranking=TRUE)
+
+normalize=function(x){
+  return((x-min(x))/(max(x)-min(x)))}
+
+ssgseaOut=normalize(ssgseaScore)
+ssgseaOut=rbind(id=colnames(ssgseaOut),ssgseaOut)
+write.table(ssgseaOut,file="ssgseaOut.txt",sep="\t",quote=F,col.names=F)
